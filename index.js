7043f618c269a66bc2ac8a4adf43bd188e65d93b
Mohammed Rakib <mohammedrakib81336@gmail.com>
2021-01-26 19:31:51 +0600

    third commit

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const loginBtn = document.getElementById("login-btn");
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display="block"
 });
+// deposit button event handler
+
+const deposit = document.getElementById("deposit");
+deposit.addEventListener("click",function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+    document.getElementById("depositAmount").value= "";
+})
