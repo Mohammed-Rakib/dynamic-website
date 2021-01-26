cbb590eb258d08b414fa8861226c12c5ab151d1e
Mohammed Rakib <mohammedrakib81336@gmail.com>
2021-01-26 19:57:36 +0600

    final commit

--- index.js
+++ index.js
@@ -17,5 +17,34 @@ deposit.addEventListener("click",function(){
     const totalDeposit = depositNumber + currentDepositNumber;
     
     document.getElementById("currentDeposit").innerText = totalDeposit;
+
+
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document.getElementById("currentBalance").innerText = totalBalance;
+
+
     document.getElementById("depositAmount").value= "";
+})
+
+// withdraw button event handler
+
+const withdraw = document.getElementById("withdraw");
+withdraw.addEventListener("click",function(){
+    const withdrawAmount = document.getElementById("withdrawAmount").value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const currentWithdraw = document.getElementById("currentWithdraw").innerText;
+    const currentWithdrawNumber = parseFloat(currentWithdraw);
+    const totalWithdraw = withdrawNumber + currentWithdrawNumber;
+    
+    document.getElementById("currentWithdraw").innerText = totalWithdraw;
+ 
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance =currentBalanceNumber - totalWithdraw;
+    document.getElementById("currentBalance").innerText = totalBalance;
+
+    document.getElementById("withdrawAmount").value= "";
 })
